f76fd9c534b3ff1262f87e877af41d8ca9d8d52f
Palina Harokh <palina_harokh@epam.com>
2023-04-04 22:27:21 +0300

    Task 3: add webpack

---
+++ product-service/webpack.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack')
+const serverless = require('serverless-webpack');
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = (async () => {
+    return {
+        entry: serverless.lib.entries,
+        target: "node",
+        mode: serverless.lib.webpack.isLocal ? "development" : "production",
+        externals: [nodeExternals()],
+        module: {
+            rules: [{ test: /\.js$/, use: "babel-loader" }],
+        },
+    };
+})();
